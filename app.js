e3145830b46772ae7af7f2290234b179cc7f6f4b
HukMux1 <59342589+HukMux1@users.noreply.github.com>
2021-03-14 23:45:14 -0500

    Commit to test the full Pipeline in AWS

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(' TEST APP IN JS FOR HKMT CI/CD PIPELINE '));
+app.get('/', (req, res) => res.send(' TEST APP IN JS FOR HKMT CI/CD PIPELINE 
+
+
+    Mas Text added here... Prep for Manual Review
+
+
+'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
